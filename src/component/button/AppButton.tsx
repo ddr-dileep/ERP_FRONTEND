1c4fa5b061d889501867f665d993984d32faad5f
Dileep <ddr.dileepraloti@gmail.com>
2024-09-09 17:35:33 +0530

    feat : common component

--- src/component/button/AppButton.tsx
+++ src/component/button/AppButton.tsx
@@ -1,3 +1,30 @@
-export const AppButton = () => {
-  return <div>AppButton</div>;
+import { MouseEventHandler } from "react";
+
+export const AppButton = ({
+  title,
+  onClick,
+  onDoubleClick,
+  className,
+  id,
+}: IButton) => {
+  return (
+    <button
+      onClick={onClick}
+      onDoubleClick={onDoubleClick}
+      className={`app-button ${className}`}
+      data-testid={id}
+      id={id}
+      type="button"
+    >
+      {title}
+    </button>
+  );
 };
+
+interface IButton {
+  title: string;
+  onClick: MouseEventHandler<HTMLButtonElement> | undefined;
+  onDoubleClick?: MouseEventHandler<HTMLButtonElement> | undefined;
+  className?: string;
+  id?: string;
+}
--- src/component/input/AppInput.tsx
+++ src/component/input/AppInput.tsx
@@ -1,3 +1,41 @@
-export const AppInput = () => {
-  return <div>AppInput</div>;
+import "./input.scss";
+
+export const AppInput = ({
+  type = "text",
+  onChange = () => {},
+  value,
+  placeholder,
+  className,
+  required,
+  id,
+  ...rest
+}: IAppInput) => {
+  return (
+    <input
+      type={type}
+      value={value}
+      onChange={onChange}
+      placeholder={placeholder}
+      className={`app-input ${className}`}
+      required={required}
+      autoComplete="off"
+      spellCheck="false"
+      autoCapitalize="off"
+      autoCorrect="off"
+      autoSave="off"
+      data-testid={id}
+      id={id}
+      {...rest}
+    />
+  );
 };
+
+interface IAppInput {
+  type: string;
+  value: any;
+  onChange: (e: React.FormEvent<HTMLInputElement>) => string | undefined | void;
+  placeholder?: string;
+  className?: string;
+  required?: boolean;
+  id: string;
+}
