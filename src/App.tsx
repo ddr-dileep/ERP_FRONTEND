8a883c11d1c576c214cdd8fe8ce41a32ce4235ee
Dileep <ddr.dileepraloti@gmail.com>
2024-09-09 12:44:03 +0530

    fix : setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import "./App.css";
-
 function App() {
-  return <></>;
+  return <>App</>;
 }
 
 export default App;
---
+++ src/component/input/AppInput.tsx
@@ -0,0 +1,3 @@
+export const AppInput = () => {
+  return <div>AppInput</div>;
+};
